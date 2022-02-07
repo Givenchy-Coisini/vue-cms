cb5a38e6d5655b001a702b4db4634fcf8679778c
Givenchy-Coisini <2312696405@qq.com>
2022-02-07 22:52:49 +0800

    feat(eslint): 忽略types规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,5 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off'
-
   }
 }
